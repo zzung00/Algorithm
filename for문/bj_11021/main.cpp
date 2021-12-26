5ccb28d71524ae204e03cc8b773a853e43665de1
zzung00 <lee759444@gmail.com>
2021-12-26 16:03:42 +0900

    complete 11021

---
+++ for문/bj_11021/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, len;
+
+	cin >> len;
+
+	for (int i = 1; i <= len; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << "\n";
+	}
+
+	return 0;
+}
