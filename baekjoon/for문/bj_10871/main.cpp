42b6073c822c53199e1cece900ef55b04cecc4ae
zzung00 <lee759444@gmail.com>
2022-01-01 17:23:05 +0900

    complete 10871

--- baekjoon/for문/bj_10871/main.cpp
+++ baekjoon/for문/bj_10871/main.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main() {
+	int n, x, num;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> n >> x;
 
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+
+		if (num < x)
+		{
+			cout << num << "\n";
+		}
+	}
 
 	return 0;
 }
