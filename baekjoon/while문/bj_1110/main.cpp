d167e8ec0515a4b91462a2de32fd6b6cca438eae
zzung00 <lee759444@gmail.com>
2022-01-02 13:56:01 +0900

    complete 1110

--- baekjoon/while문/bj_1110/main.cpp
+++ baekjoon/while문/bj_1110/main.cpp
@@ -3,7 +3,29 @@
 using namespace std;
 
 int main() {
-	int n;
-	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m, left, right;
+	int i = 1;
+
+	cin >> n;
+
+	m = n;
+
+	while (true)
+	{
+		left = m / 10;
+		right = m % 10;
+		m = right * 10 + ((left + right) % 10);
+
+		if (n == m)
+		{
+			break;
+		}
+		i++;
+	}
+	cout << i << "\n";
+
 	return 0;
 }
