0516361c8ff9d09ebfa16cb9b2b9bd120cfa8ac2
zzung00 <lee759444@gmail.com>
2022-01-21 23:04:47 +0900

    complete bj_11654

---
+++ baekjoon/문자열/bj_11654/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	char ascii;
+	
+	cin >> ascii;
+	cout << (int)ascii << "\n";
+
+	return 0;
+}
