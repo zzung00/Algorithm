52f569794dfa1c9b27c14fec1108f840477d59b6
zzung00 <lee759444@gmail.com>
2022-01-29 14:55:12 +0900

    complete bj_11720

--- baekjoon/문자열/bj_11720/main.cpp
+++ baekjoon/문자열/bj_11720/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>
+#include <numeric>
 
 using namespace std;
 
 int main() {
-    cout << "hee" << "\n";
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += (str[i] - '0');
+    }
+    cout << sum << "\n";
 
     return 0;
 }
