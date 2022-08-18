fbbdb265b9ad71a40d6c837f2aae52eb89eaa2a9
zzung00 <lee759444@gmail.com>
2022-08-18 19:08:33 +0900

    complete boj_2751

---
+++ baekjoon/정렬/boj_2751/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+
+    for (int i : v)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
