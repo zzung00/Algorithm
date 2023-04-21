4c1c4da82e1daddadd7e9ac649c40ff1505d58a9
zzung00 <lee759444@gmail.com>
2023-04-21 18:58:03 +0900

    complete boj_2743

---
+++ baekjoon/문자열/boj_2743/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string str;
+    cin >> str;
+    cout << str.size() << "\n";
+
+    return 0;
+}
