ae2388cbbd1e9bd5dc2dd1c33ba84809e8f423e1
zzung00 <lee759444@gmail.com>
2023-04-22 18:26:47 +0900

    complete boj_9095

---
+++ baekjoon/DP/boj_9095/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t, n;
+    cin >> t;
+    vector<int> dp(12, 0);
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for (int i = 4; i < 12; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        cout << dp[n] << "\n";
+    }
+        
+    return 0;
+}
