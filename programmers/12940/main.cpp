e910abee4cd9cca1f4078a79ae68a508759ff944
zzung00 <lee759444@gmail.com>
2023-06-17 14:46:08 +0900

    complete 12940_최대공약수와 최소공배수

---
+++ programmers/12940/main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <numeric>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    
+    answer.push_back(gcd(n, m));
+    answer.push_back(lcm(n, m));
+    
+    return answer;
+}
+
+int main() {
+    int n = 3;
+    int m = 12;
+    vector<int> result = solution(n, m);
+
+    for (int i : result)
+    {
+        cout << i << " ";
+    }
+    cout << "\n";
+}
