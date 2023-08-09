643e1de3641e8f6d77ec603bd812dd80bb20a283
zzung00 <lee759444@gmail.com>
2023-08-09 15:45:17 +0900

    complete 87390_n^2 배열 자르기

---
+++ programmers/87390/main.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    
+    for (long long i = left; i <= right; i++)
+    {
+        int row = i / n;
+        int col = i % n;
+        
+        answer.push_back(max(row, col) + 1);
+    }
+    
+    return answer;
+}
+
+int main() {
+    int n = 3;
+    long long left = 2;
+    long long right = 5;
+    vector<int> result = solution(n, left, right);
+
+    for (int i : result)
+    {
+        cout << i << " ";
+    }
+    cout << "\n";
+}
