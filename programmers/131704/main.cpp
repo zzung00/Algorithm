eb5d8d75781ef96f867f55aa8be331aa71b42977
zzung00 <lee759444@gmail.com>
2023-09-24 21:37:35 +0900

    complete 131704_택배상자

---
+++ programmers/131704/main.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> order) {
+    int answer = 0;
+    stack<int> s;
+    
+    for (int i = 1; i <= order.size(); i++)
+    {
+        s.push(i);
+        
+        while (!s.empty() && s.top() == order[answer])
+        {
+            s.pop();
+            answer++;
+        }
+    }
+    return answer;
+}
+
+int main() {
+    vector<int> order = {4, 3, 1, 2, 5};
+    cout << solution(order) << "\n";
+}
