e6fcd0fff7b5bdbd6de5301db22c2452ddefc919
zzung00 <lee759444@gmail.com>
2024-01-04 17:05:29 +0900

    complete 120804

---
+++ programmers/120804/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    answer = num1 * num2;
+    return answer;
+}
+
+int main() {
+    cout << solution(3, 4) << "\n";
+}
